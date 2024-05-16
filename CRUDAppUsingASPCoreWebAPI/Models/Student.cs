d860b6311fa84929e82f53e74dfb92fe40020976
Admin <Admin@AZIZ-MHR>
2024-05-16 16:12:15 +0500

    Add project files.

---
+++ CRUDAppUsingASPCoreWebAPI/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAppUsingASPCoreWebAPI.Models
+{
+
+    public class Student
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Addres { get; set; }
+    }
+
+}
